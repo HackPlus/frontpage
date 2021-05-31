4ad63848e2feda8c9c0a3aa7540390982050e1b5
eilla1 <git@ella.cx>
2021-05-31 16:51:51 -0700

    feat: add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#007bff",
+        cyan: "#17a2b8",
+        red: "#dc3545",
+        dark: "#212529",
+        light: "#f8f9fa",
+        gray: "#6c757d",
+        graydark: "#343a40",
+        green: "#28a745",
+        indigo: "#6610f2",
+        orange: "#fd7e14",
+        pink: "#e83e8c",
+        yellow: "#ffc107",
+        white: "#ffffff",
+      },
+      fontFamily: {
+        circular: "CircularStd",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
