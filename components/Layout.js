4b57f3470fb595eaff660b7ded54bd3e324780a9
Ella <72365100+eilla1@users.noreply.github.com>
2021-06-07 15:47:55 -0700

    feat: set up mdx (#4)

--- components/Layout.js
+++ components/Layout.js
@@ -2,13 +2,27 @@ import Footer from "./Footer";
 import Meta from "./Meta";
 import Nav from "./Nav";
 
-export default function Layout({ title, children }) {
-  return (
-    <div>
-      <Meta title={title} />
-      <Nav />
-      <div>{children}</div>
-      <Footer />
-    </div>
-  );
+export default function Layout({ layout, title, children }) {
+  switch (layout) {
+    case "document":
+      return (
+        <div>
+          <Meta title={title} />
+          <Nav />
+          <div className="mx-8 md:mx-24 lg:mx-48 my-8">{children}</div>
+          <Footer />
+        </div>
+      )
+    default:
+      return (
+        <div>
+          <Meta title={title} />
+          <Nav />
+          <div>{children}</div>
+          <Footer />
+        </div>
+      )
+  }
 }
+
+// use `layout="document"` for the text pages or pages that don't have other designs like svgs because there's a default margin
