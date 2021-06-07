958c9d9f1dc673f7fa875ca550b9c09ff6700b15
eilla1 <git@ella.cx>
2021-06-07 15:47:07 -0700

    feat: set up mdx

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import { MDXProvider } from "@mdx-js/react";
+import MDXComponents from "../components/MDXComponents";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MDXProvider components={MDXComponents}>
+      <Component {...pageProps} />
+    </MDXProvider>
+  );
 }
 
 export default MyApp;
