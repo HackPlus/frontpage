bc39ecf8c6f9e39e6ebd034a3031e56af77a9c60
eilla1 <git@ella.cx>
2021-06-07 18:40:15 -0700

    carousel but broken

---
+++ components/Carousel/Slider.js
@@ -0,0 +1,80 @@
+// import { CarouselData } from "./CarouselData";
+import { useState } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import CarouselItem from "./CarouselItem";
+
+const CarouselData = [
+  {
+    image: "/karinahalevy.png",
+    excerpt:
+      "Hack+ has been instrumental not only in making both of the LingHacks hackathons possible but also enabling us to expand into an international organization reaching hundreds of students.",
+    testimonial:
+      "When I initially came up with the idea of LingHacks, I had no clue where to start in terms of getting nonprofit status, handling legal paperwork, or raising and storing funds--Hack+ managed all of that seamlessly and made it possible for my team and me to focus on outreach and curriculum. They made it super easy to invoice sponsors, keep track of attendee registrations, and communicate with my team. My team and I are eternally grateful to Hack+ for getting us where we are today!",
+    name: "Karina Halevy",
+    role: "Executive Director",
+    eventName: "LingHacks",
+  },
+  {
+    image: "/scott.png",
+    excerpt:
+      "Garuda Hacks wouldn't have been possible without the Hack+ infrastructure that abstracted away the complexity of raising sponsorship and making vendor payments.",
+    testimonial:
+      "Garuda Hacks was organized at the very last moment with only 2.5 months          left in our hands, and it wouldn't have been possible without the Hack+ infrastructure that abstracted away the complexity of raising sponsorship and making vendor payments. As a result,          we were able to put more energy into logistics, marketing, and hacker experience, making          Garuda Hacks Indonesia's largest hackathon, with 2500+ participants, and receiving the          support of Indonesia's Ministry of Tourism and Creative Economy.",
+    name: "Scott Moses Sunarto",
+    role: "Managing Director",
+    eventName: "Garuda Hacks",
+  },
+  {
+    image: "/eileenyang.jpg",
+    excerpt:
+      "Working with Hack+ last fall was an astoundingly rewarding decision.",
+    testimonial:
+      "Hack+ handled all the legal and financial duties, from liability waivers to accounting and even registration, allowing all of our HSHacks organizers to focus on creating the most optimal experience we possibly could for all of our hackers. Thank you Hack+!",
+    name: "Eileen Yang",
+    role: "Lead Director",
+    eventName: "HSHacks",
+  },
+];
+
+const Carousel = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  return (
+    <div className="relative flex items-center justify-center">
+      <FaArrowLeft onClick={prevSlide} />
+      <FaArrowRight onClick={nextSlide} />
+      {CarouselData.map((index, slide) => {
+        return (
+          <div
+            key={index}
+            // className={index === current ? "slide active" : "slide"}
+          >
+            <CarouselItem
+              image={slide.image}
+              excerpt={slide.excerpt}
+              testimonial={slide.testimonial}
+              name={slide.name}
+              eventName={slide.eventName}
+              role={slide.role}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Carousel;
