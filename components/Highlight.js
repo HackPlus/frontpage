840fbe4e0f9598ee9b7820bcc0ff829cb809ab63
eilla1 <git@ella.cx>
2021-06-08 01:21:05 -0700

    feat: events page

--- components/Highlight.js
+++ components/Highlight.js
@@ -15,15 +15,15 @@ export default function Highlight() {
         <Badge fillcolor="purple">All-Female</Badge>
         <Badge fillcolor="green">Collegiate</Badge>
       </div>
-      <div className="flex flex-col space-y-3">
-        <h4>TechTogether Boston</h4>
-        <p className="text-sm">
+      <div className="flex flex-col space-y-5">
+        <h4 className="text-2xl font-medium">TechTogether Boston</h4>
+        <p className="text-base">
           {" "}
           The world&apos;s largest all-female and non-binary hackathon, at
           Agganis Arena in Boston.{" "}
         </p>
         <Link href="/">
-          <a className="text-sm font-semibold">Visit the website</a>
+          <a className="text-base font-semibold">Visit the website &rarr;</a>
         </Link>
       </div>
     </div>
