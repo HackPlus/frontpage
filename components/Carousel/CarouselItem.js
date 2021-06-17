0818134d4c8e22f5c39a61732ad72533cece988d
eilla1 <git@ella.cx>
2021-06-17 00:31:15 -0700

    chore: style carousel

--- components/Carousel/CarouselItem.js
+++ components/Carousel/CarouselItem.js
@@ -7,19 +7,19 @@ export default function CarouselItem({
   role,
 }) {
   return (
-    <div className="border-2 border-graylight rounded-xl w-9/12 md:w-7/12 mx-auto">
-      <div className="flex flex-row space-x-5 relative z-0">
-        <div className="w-1/6">
+    <div className="border-2 border-graylight rounded-xl w-9/12 md:w-7/12 mx-auto p-6">
+      <div className="grid grid-cols-8 space-x-5 relative z-0">
+        <div className="col-span-1">
           <img
             src={image}
             alt="Profile picture"
-            className="border-2 border-graylight w-48 h-48 rounded-full"
+            className="border-2 border-graylight rounded-full"
           />
         </div>
-        <div className="flex flex-col space-y-2">
-          <p className="text-purple ">{excerpt}</p>
-          <p className="text-gray">{testimonial}</p>
-          <p className="text-black">{name}</p>
+        <div className="flex flex-col space-y-2 col-span-7">
+          <p className="text-purple text-xl leading-tight">&quot;{excerpt}</p>
+          <p className="text-gray">{testimonial}&quot;</p>
+          <p className="text-black text-2xl pt-3 leading-4">{name}</p>
           <p className="text-gray">
             {role}, <span className="text-purple">{eventName}</span>
           </p>
