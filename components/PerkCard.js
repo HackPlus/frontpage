1cf27ccc82c788751ab99f6c8a261d5b10a3d81e
eilla1 <git@ella.cx>
2021-06-27 22:46:45 -0700

    fix: even card heights

--- components/PerkCard.js
+++ components/PerkCard.js
@@ -1,13 +1,13 @@
 export default function Perk({ heading, description, children }) {
-    return (
-        <div className="border-2 border-graylight bg-white rounded-xl px-3 py-4 py-auto">
-            <div className="flex flex-row space-x-3 items-center">
-                {children}
-                <div className="flex flex-col">
-                    <p className="text-xl">{heading}</p>
-                    <p className="text-gray text-base">{description}</p>
-                </div>
-            </div>
+  return (
+    <div className="border-2 border-graylight bg-white h-full rounded-xl px-3 py-4 m-auto">
+      <div className="flex flex-row space-x-6 items-center">
+        <div className="mx-2">{children}</div>
+        <div className="flex flex-col">
+          <p className="text-xl">{heading}</p>
+          <p className="text-gray text-base">{description}</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
